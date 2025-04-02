7af2686a4e4c9884c7103e8aa710119c3f81ec5f
Mateus Oliveira <131485645+mateusix@users.noreply.github.com>
2025-04-02 20:17:04 -0300

    Subindo os arquivso do Portfolio

---
+++ script.js
@@ -0,0 +1,33 @@
+// Seleção dos elementos
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+// Ativar link ao rolar a página
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY; // Correção: scrollY com "Y" maiúsculo
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+      });
+
+      let activeLink = document.querySelector('header nav a[href*="' + id + '"]');
+      if (activeLink) {
+        activeLink.classList.add('active');
+      }
+    }
+  });
+};
+
+// Abrir/fechar menu no clique
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x'); // Alterna o ícone
+  navbar.classList.toggle('active'); // Mostra/oculta o menu
+};
